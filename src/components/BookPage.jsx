1e6b10bc49c5f94ca8af4c9f0d0d1fd772f51943
whqjagml <dkfcks15234@naver.com>
2023-04-15 11:35:31 +0900

    ver-02

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -4,22 +4,24 @@ import { Button, Col, Row, Form, Card } from 'react-bootstrap'
 import Book from './Book';
 
 const BookPage = () => {
-    const [list,setList] = useState([]);
-    const [loading, setLoding] = useState(false);
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
     const [is_end, setIs_end] = useState(false);
-    const [query, setQuery] = useState('리액터')
+    const [query, setQuery] = useState('리액트');
+
     const getData = async() => {
         const url="https://dapi.kakao.com/v3/search/book?target=title";
         const config={
-            headers: {"Authorization": "KakaoAK bd089b1ebcad6ba7757c9127960c80a8 "},
+            headers: {"Authorization": "KakaoAK b80880fbde422de3fd9b4a4e67c9bb54"},
             params: {query: query, page:page, size:8}
         }
-        const result= await axios.get(url, config); //async,await
+        setLoading(true);
+        const result= await axios.get(url, config);
         console.log(result);
-        setList(result.data.documents); //데이터 출력 //ellipsis 한줄나오고 쩜쩜쩜
+        setList(result.data.documents);
         setIs_end(result.data.meta.is_end);
-        setLoding(false);
+        setLoading(false);
     }
 
     const onSubmit = (e) => {
@@ -27,44 +29,47 @@ const BookPage = () => {
         setPage(1);
         getData();
     }
-    useEffect (()=>{ //useEffect
+
+    useEffect(()=>{
         getData();
     }, [page]);
-    if (loading) return <h1 className='text-center my-5'>로딩중.......</h1>
+
+    if(loading) return <h1 className='text-center my-5'>로딩중......</h1>
     return (
         <Row>
             <h1 className='text-center my-5'>도서검색</h1>
             <Row>
                 <Col md={4}>
-                <Form onSubmit={onsubmit}>
-                    <Form.Control 
-                    onChange={(e)=>setQuery(e.target.value)}
-                    placeholder="검색어" value={query}/>
+                    <Form onSubmit={onSubmit}>
+                        <Form.Control  
+                            onChange={(e)=>setQuery(e.target.value)}
+                            placeholder="검색어" value={query}/>
                     </Form>
-                    </Col>
-                {list.map(book=>
-                <Col key={book.isbn} md={3} xs={6} className="my-2">
-                <Card>
-                    <Card.Body>
-                        <img src={book.thumbnail}/>
-                        <div className='ellipsis'>{book.title}</div> 
-                        <Book book={book}/>
-                    </Card.Body>
-                </Card>
                 </Col>
-                    )}
-                    <div className='text-center my-3'>
-                        <Button onClick={()=>setPage(page-1)}
+            </Row>
+            <Row>
+                {list.map(book=>
+                    <Col key={book.isbn} md={3} xs={6} className="my-2">
+                        <Card>
+                            <Card.Body>
+                                <img src={book.thumbnail}/>
+                                <div className='ellipsis'>{book.title}</div>
+                                <Book book={book}/>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )}
+                <div className='text-center my-3'>
+                    <Button onClick={()=>setPage(page-1) }
                         disabled = {page==1 && true}
-                        Button className="btn-sm">이전</Button>
-                        <span className="px-3">{page}</span>
-                        <Button onClick={()=>setPage(page+1)}
+                        className="btn-sm">이전</Button>
+                    <spna className="px-3">{page}</spna>
+                    <Button onClick={()=>setPage(page+1) }
                         disabled = {is_end && true}
-                         Button className="btn-sm">다음</Button>
-                        </div> 
+                        className="btn-sm">다음</Button>
+                </div>
             </Row>
         </Row>
-
     )
 }
 
